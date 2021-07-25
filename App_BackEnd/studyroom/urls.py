be03bb5a05204e13e9cf16af7928e3247703c0cf
dahyun <dahyun_ee@naver.com>
2021-07-25 13:29:35 +0900

    FEAT /studyroom/

--- App_BackEnd/studyroom/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^studyroom/$', views.studyroom.as_view(), name='studyroom'),
-    url(r'^studyroom/password/$', views.studyroom_pw.as_view(), name='studyroom_pw'),
-]
