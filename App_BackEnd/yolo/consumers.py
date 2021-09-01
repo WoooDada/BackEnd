28b6adc0359b4076e5ddd3d43cffb3f5a6df0da1
dahyun <dahyun_ee@naver.com>
2021-09-01 23:19:14 +0900

    FIX /study/

--- App_BackEnd/yolo/consumers.py
+++ App_BackEnd/yolo/consumers.py
@@ -3,21 +3,20 @@ import json
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 from asgiref.sync import async_to_sync
 
-class sendConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+class sendConsumer(WebsocketConsumer):
+    def connect(self):
         print("connected")
+        self.accept()
 
 
-    async def disconnect(self, code):
+    def disconnect(self, code):
         print("disconnected")
 
 
-"""
-    async def receive(self, text_data):
+    def receive(self, text_data):
         data = json.loads(text_data)
         message = data['message']
 
         self.send(text_data=json.dumps({
             'message': message
         }))
-"""
