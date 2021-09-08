1fba54ed8dc57a5afeebc57181c25c545f58c95c
dahyun <dahyun_ee@naver.com>
2021-09-08 22:27:41 +0900

    FIX /yolo/consumers.py

--- App_BackEnd/yolo/consumers.py
+++ App_BackEnd/yolo/consumers.py
@@ -28,7 +28,8 @@ class sendConsumer(WebsocketConsumer):
     def receive(self, text_data):
         data = json.loads(text_data)
         message = data['message']
-        nickname = data['nickname'].decode('utf-8')
+        nickname = data['nickname']
+        nickname = str(nickname, "utf-8")
 
         file = base64_file(message, name='yolo_picture')
         file.name = nickname + ".png"
