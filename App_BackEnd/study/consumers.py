88ccaa706d829077959fbc24f05776deed4d5601
dahyun <dahyun_ee@naver.com>
2021-09-27 17:29:03 +0900

    FEAT socket delete관련 체크

--- App_BackEnd/study/consumers.py
+++ App_BackEnd/study/consumers.py
@@ -118,51 +118,42 @@ class sendMate(AsyncWebsocketConsumer):
 
                 else:  # 다른 사람들
 
-                    if Daily_1m_content.objects.filter(uid=user).exists():
-                        study_info = Daily_1m_content.objects.filter(uid=user)
-                        concent = 0
-                        play = 0
+                    study_info = Daily_1m_content.objects.filter(uid=user)
+                    concent = 0
+                    play = 0
 
-                        for info in study_info:  # 실시간 play/concent 개수 가져오기
-                            if info.type == 'C':
-                                concent += 1
-                            elif info.type == 'P':
-                                play += 1
+                    for info in study_info:  # 실시간 play/concent 개수 가져오기
+                        if info.type == 'C':
+                            concent += 1
+                        elif info.type == 'P':
+                            play += 1
 
-                        tot_time = study_info.count()
-                        if concent == 0:
-                            concent_rate = '0'
-                        else:
-                            concent_rate = round(concent / tot_time, 2) * 100
+                    tot_time = study_info.count()
+                    if concent == 0:
+                        concent_rate = '0'
+                    else:
+                        concent_rate = round(concent / tot_time, 2) * 100
 
-                        concent_time = get_time(concent)
-                        if int(concent_time.split(":")[0]) == 0:
-                            concent_time = concent_time.split(":")[1] + "분"
+                    concent_time = get_time(concent)
+                    if int(concent_time.split(":")[0]) == 0:
+                        concent_time = concent_time.split(":")[1] + "분"
 
-                        else:
-                            concent_time = concent_time.split(":")[0] + "시간 " + concent_time.split(":")[1] + "분"
+                    else:
+                        concent_time = concent_time.split(":")[0] + "시간 " + concent_time.split(":")[1] + "분"
 
-                        play_time = get_time(play)
-                        if int(play_time.split(":")[0]) == 0:
-                            play_time = play_time.split(":")[1] + "분"
-                        else:
-                            play_time = play_time.split(":")[0] + "시간 " + play_time.split(":")[1] + "분"
+                    play_time = get_time(play)
+                    if int(play_time.split(":")[0]) == 0:
+                        play_time = play_time.split(":")[1] + "분"
+                    else:
+                        play_time = play_time.split(":")[0] + "시간 " + play_time.split(":")[1] + "분"
 
-                        if user.uid != uid:
-                            studymates.append({
-                                "nickname": user.nickname,
-                                "concent_rate": str(concent_rate) + "%",
-                                "concent_time": concent_time,
-                                "play_time": play_time
-                            })
-                    else :
+                    if user.uid != uid:
                         studymates.append({
                             "nickname": user.nickname,
-                            "concent_rate": "0.0%",
-                            "concent_time": 0,
-                            "play_time": 0
+                            "concent_rate": str(concent_rate) + "%",
+                            "concent_time": concent_time,
+                            "play_time": play_time
                         })
-
 
             await self.send(
                 text_data=json.dumps({
