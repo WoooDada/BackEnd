12fe0ca6166ba16c974c4597404aa616d3f957bb
dahyun <dahyun_ee@naver.com>
2021-09-27 22:21:24 +0900

    FEAT issecret

--- App_BackEnd/studyroom/views.py
+++ App_BackEnd/studyroom/views.py
@@ -12,7 +12,8 @@ def append_data(data, room):
                     "maxppl":room.maxppl,
                     "inppl":room.f_room.all().count(),
                     "room_comment":room.room_comment,
-                    "issecret":room.is_secret,
+                  #  "issecret":room.is_secret,
+                    "room_issecret": room.is_secret,
                     "room_tag":room.room_tag,
                     "room_color":room.room_color
                 })
